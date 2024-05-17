75e1bece231f4fef26ab5408b9a3b90ae83a0dce
Vidit210333 <114834132+Vidit210333@users.noreply.github.com>
2024-05-17 13:25:49 +0530

    Update emotionaudio_app.py

--- emotionaudio_app.py
+++ emotionaudio_app.py
@@ -10,19 +10,18 @@ import streamlit as st
 from PIL import Image
 import pickle
 import requests
+import gdown
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 
+# Function to download file from Google Drive using gdown
 def download_file_from_google_drive(file_id, destination):
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    drive = GoogleDrive(gauth)
-    file_obj = drive.CreateFile({'id': file_id})
-    file_obj.GetContentFile(destination)
-    
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, destination, quiet=False)
+
+# Define the model ID and path
 model_id = "1Lf9QhN7DMf8WnVYYgjEmwZ1kmbc-U0Dt"
 model_path = "/tmp/model.pickle"
+
 # Download the model file from Google Drive
 download_file_from_google_drive(model_id, model_path)
 # from keras.models import load_model
